7821170af3e3858043d35ec152676d6e9c396b72
msaijagapathi <warcommander2000@gmail.com>
2025-12-18 15:43:21 +0530

    path

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      base: "/Jewellery_Flow/", // Add this line
+      base: "/", // Add this line
       plugins: [react()],
       resolve: {
         alias: {
